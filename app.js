1e373177989ac98059be7abc4fc800a00d9ceb3b
Ömer Can <omercanhocaoglu@gmail.com>
2024-03-29 16:22:49 +0300

    Models oluşturuldu.

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 //Template engine
 app.set("view engine", "ejs");
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
 
 // Routes
-app.get( "/", ( req, res ) => {
-    res.render('index');
+app.get( "/", async ( req, res ) => {
+    const photos = await Photo.find({});
+    res.render('index', {
+        photos
+    });
 });
 app.get( "/about", ( req, res ) => {
     res.render('about');
 });
 app.get( "/add", ( req, res ) => {
     res.render('add');
+});
+app.post( "/photos", async ( req, res ) => {
+    // console.log(req.body);
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 const port = 3000;
